38fb030d5b2f93898dbab189acbbfc165ef5f58b
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-20 19:23:07 +0400

    Add files via upload

---
+++ Drawing-4/Drawing-4/Drawing_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_4App.swift
+//  Drawing-4
+//
+//  Created by Abdurokhman Mukhanbetov on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
